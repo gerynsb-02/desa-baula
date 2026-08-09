3e20b112cc66c0b1bc7bb28077d4a3ef9ae4664a
gerynsb-2 <deangerypasamba@gmail.com>
2026-08-09 14:01:39 +0800

    feat: add custom document component with SEO meta tags, favicon, and structured schema data

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,10 @@ export default function Document() {
     <Html lang="id">
       <Head>
         {/* Favicon */}
-        <link rel="icon" href="/images/logo.png" />
-        <link rel="apple-touch-icon" href="/images/logo.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/logo.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/logo.png" />
+        <link rel="shortcut icon" href="/images/logo.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/logo.png" />
         <link rel="manifest" href="/manifest.json" />
         
         {/* Meta Tags */}
@@ -23,15 +25,22 @@ export default function Document() {
         {/* Theme Color */}
         <meta name="theme-color" content="#16a34a" />
         <meta name="msapplication-TileColor" content="#16a34a" />
+        <meta name="msapplication-TileImage" content="/images/logo.png" />
         
         {/* Open Graph Default */}
         <meta property="og:type" content="website" />
         <meta property="og:site_name" content="Desa Baula" />
         <meta property="og:locale" content="id_ID" />
+        <meta property="og:image" content="https://desabaula.site/images/logo.png" />
+        <meta property="og:image:width" content="512" />
+        <meta property="og:image:height" content="512" />
+        <meta property="og:image:alt" content="Logo Desa Baula" />
         
         {/* Twitter Card Default */}
-        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@desa_baula" />
+        <meta name="twitter:image" content="https://desabaula.site/images/logo.png" />
+        <meta name="twitter:image:alt" content="Logo Desa Baula" />
         
         {/* Structured Data - Organization */}
         <script
